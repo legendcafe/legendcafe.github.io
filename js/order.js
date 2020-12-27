c7c1d773a8ba9b59b43ba249d5b6ef1cc38306a8
hongsea <hongsea119@gmail.com>
2020-12-27 20:44:59 +0700

    update

---
+++ js/order.js
@@ -0,0 +1,13 @@
+var getsize;
+var getsize1;
+
+function getInfo(size, unitPrice){
+    alert(size);
+    var getsize = size;
+
+}
+
+function showInfo(){
+    getsize1 = document.getElementById("getData").innerHTML = getsize;
+    console.log(getsize);
+}
